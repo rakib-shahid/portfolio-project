5e1e070c55d62c9df397ce8e99019752c48d4b90
rakib-shahid <rakibshahid1298@gmail.com>
2025-03-12 23:51:03 -0400

    updated routes

--- my-portfolio/src/app/blog/page.js
+++ my-portfolio/src/app/blog/page.js
@@ -1,10 +1,12 @@
 import Link from "next/link";
+import { headers } from "next/headers";
 
 async function getPosts() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/posts`);
+  const host = headers().get("host");
+  const protocol = host?.includes("localhost") ? "http" : "https";
+  const res = await fetch(`${protocol}://${host}/api/posts`);
   return res.json();
 }
-
 export default async function BlogPage() {
   const posts = await getPosts();
   return (
